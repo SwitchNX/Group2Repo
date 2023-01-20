e012582f194096d3cb9fa73f3e73295d964d5c55
Hudson Ward <htw6676@rit.edu>
2023-01-20 16:54:49 -0500

    Got the palindrome code mostly working.

--- PEs/GroupVersionControl/Program.cs
+++ PEs/GroupVersionControl/Program.cs
@@ -9,6 +9,40 @@ namespace GroupVersionControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Palindrome Checker
+
+            Console.WriteLine("Enter a word:");
+            String checkPalindrome = Console.ReadLine();
+            checkPalindrome = checkPalindrome.Trim().ToLower();
+            Boolean palindromeResult = false;
+            int wordLength = checkPalindrome.Length;
+            int halfLength = 0;
+
+            if (wordLength != 0)
+            {
+                halfLength = wordLength / 2;
+                for (int i = 0; i < halfLength; i++)
+                {
+                    if (checkPalindrome.Substring(i, 1).Equals(checkPalindrome.Substring(wordLength - (i + 1))) == true)
+                    {
+                        palindromeResult = true;
+                        wordLength--;
+                    } else
+                    {
+                        palindromeResult = false;
+                        break;
+                    }
+                }
+            }
+
+            if (palindromeResult == true)
+            {
+                Console.WriteLine("That's a palindrome!");
+            } else
+            {
+                Console.WriteLine("That's not a palindrome!");
+            }
         }
     }
 }
