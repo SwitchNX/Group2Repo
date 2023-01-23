fde42f8457296af3324ddae27aa4b5bfccc854b6
Anthony Curtis <amc5224@rit.edu>
2023-01-23 16:15:17 -0500

    story generator first commit

---
+++ PEs/CollaborativeStoryGenerator_G2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Anthony Curtis, 
+// 01/23/2023
+// Generate a random story
+namespace CollaborativeStoryGenerator_G2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userChoice;
+
+            // placeholder for when we add the endings from the conflicts file
+            Console.Write("Welcome to the story generator!\n\n" +
+                          "Please choose a type of ending to generate a story:\n" +
+                          /*$*/"'{}'   '{}'   '{}'\n" +
+                          /*$*/"'{}'   '{}'   '{}'\n\n" +
+                          "Your choice >> ");
+            userChoice = Console.ReadLine()!;
+
+
+        }
+    }
+}
