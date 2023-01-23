392e81be14477a6791913e35487efbd4aeac139b
Kai Gidwani <kpg8244@rit.edu>
2023-01-23 16:26:36 -0500

    Created Actor, Conflict, and Setting classes. Added Actor.txt

---
+++ PEs/CollaborativeStoryGenerator_G2/Conflict.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollaborativeStoryGenerator_G2
+{
+    internal class Conflict
+    {
+    }
+}
