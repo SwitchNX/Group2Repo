9cc34ea13e89e1f40eaf07a19e808ff772ac0614
Kai Gidwani <kpg8244@rit.edu>
2023-01-25 12:29:58 -0500

    Finished Actor class

--- PEs/CollaborativeStoryGenerator_G2/Actor.cs
+++ PEs/CollaborativeStoryGenerator_G2/Actor.cs
@@ -4,9 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Kai Gidwani
+// 1/25/22
+// Actor class to hold the name, occupation, and a trait of an actor
+
 namespace CollaborativeStoryGenerator_G2
 {
     internal class Actor
     {
+        // Fields
+
+        private string name;
+        private string occupation;
+        private string trait;
+
+
+        // Parameters
+
+        /// <summary>
+        /// Returns the name of the character
+        /// </summary>
+        public string Name { get { return name; } }
+
+        /// <summary>
+        /// Returns the occupation of the character
+        /// </summary>
+        public string Ocupation { get { return occupation; } }
+
+        /// <summary>
+        /// Returns the trait of the character
+        /// </summary>
+        public string Trait { get { return trait; } }
+
+
+        // Constructor
+
+        /// <summary>
+        /// Creates an actor object with an inputted name, an occupation, and a trait
+        /// </summary>
+        /// <param name="name">The name of the actor</param>
+        /// <param name="occupation">The occupation of the actor</param>
+        /// <param name="trait">The trait of the actor</param>
+        public Actor(string name, string occupation, string trait)
+        {
+            this.name = name;
+            this.occupation = occupation;
+            this.trait = trait;
+        }
     }
 }
