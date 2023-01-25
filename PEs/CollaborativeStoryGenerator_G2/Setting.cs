02156c932f89de2e3b70e65d1ce1098481a591ce
Ryan Fogarty <ref2709@rit.edu>
2023-01-25 12:31:17 -0500

    Finished setting class (properly this time)

--- PEs/CollaborativeStoryGenerator_G2/Setting.cs
+++ PEs/CollaborativeStoryGenerator_G2/Setting.cs
@@ -11,40 +11,30 @@ namespace CollaborativeStoryGenerator_G2
 {
     internal class Setting
     {
-        private string attribute1;
-        private string attribute2;
-
-        /// <summary>
-        /// The first attribute of the setting
-        /// </summary>
-        public string Attribute1
-        {
-            get
-            {
-                return attribute1;
-            }
-        }
-
-        /// <summary>
-        /// The second attribute of the setting
-        /// </summary>
-        public string Attribute2
-        {
-            get
-            {
-                return attribute2;
-            }
-        }
+        Random rng = new Random();
+        private string[] firstAttributes;
+        private string[] secondAttributes;
+       
 
         /// <summary>
         /// Makes a new setting with the given attributes
         /// </summary>
         /// <param name="attribute1">The first attribute of the setting</param>
         /// <param name="attribute2">The second attribute of the setting</param>
-        public Setting(string attribute1, string attribute2)
+        public Setting(string[] firstAttributes, string[] secondAttributes)
         {
-            this.attribute1 = attribute1;
-            this.attribute2 = attribute2;
+            this.firstAttributes = firstAttributes;
+            this.secondAttributes = secondAttributes;
+        }
+
+        public string GetRandomFirstAttribute()
+        {
+            return firstAttributes[rng.Next(firstAttributes.Length)];
+        }
+
+        public string GetRandomSecondAttribute()
+        {
+            return secondAttributes[rng.Next(secondAttributes.Length)];
         }
     }
 }
