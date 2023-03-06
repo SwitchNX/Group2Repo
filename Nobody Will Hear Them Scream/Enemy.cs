cabb5974f4e8f6bd7bf13d21b3c319bf3264ac4f
Anthony Curtis <amc5224@rit.edu>
2023-03-06 15:39:23 -0500

    Added enemy, crate, and projectile classes

---
+++ Nobody Will Hear Them Scream/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobody_Will_Hear_Them_Scream
+{
+    /// <summary>
+    /// Aliens that chase the player
+    /// </summary>
+    internal class Enemy : Actor
+    {
+
+
+    }
+}
