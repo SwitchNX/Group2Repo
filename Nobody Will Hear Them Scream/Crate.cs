90bd79ad58edb663e4ea22a16e8bae9230bfd290
Hudson Ward <htw6676@rit.edu>
2023-03-06 15:44:56 -0500

    Made a player constructor and added in more using statements. (Hudson)

--- Nobody Will Hear Them Scream/Crate.cs
+++ Nobody Will Hear Them Scream/Crate.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Nobody_Will_Hear_Them_Scream
 {
--- Nobody Will Hear Them Scream/Player.cs
+++ Nobody Will Hear Them Scream/Player.cs
@@ -14,10 +14,15 @@ namespace Nobody_Will_Hear_Them_Scream
     /// </summary>
     internal class Player : Actor
     {
+        //Fields
+        private GraphicsDeviceManager _graphics;
 
-
-
-
+        //Constructor
+        public Player (Texture2D objectTexture, Rectangle objectBounds, GraphicsDeviceManager _graphics) :
+            base (objectTexture, objectBounds)
+        {
+            this._graphics = _graphics;
+        }
 
     }
 }
