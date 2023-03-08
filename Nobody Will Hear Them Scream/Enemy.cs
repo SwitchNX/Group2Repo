6a50180d938b4e2cfdaebb658bf01b33518697af
Ryan Fogarty <ref2709@rit.edu>
2023-03-08 16:28:05 -0500

    Tweaked enemy movement

--- Nobody Will Hear Them Scream/Enemy.cs
+++ Nobody Will Hear Them Scream/Enemy.cs
@@ -14,18 +14,21 @@ namespace Nobody_Will_Hear_Them_Scream
     /// </summary>
     internal class Enemy : GameObject
     {
-        //Temporarily public
+        //Fields
         private Vector2 velocity;
         private Vector2 acceleration;
-        private Vector2 playerDirFromEnemy;
 
         private Vector2 playerPosition;
+        private Vector2 playerDirFromEnemy;
+
+        private float velocityDampener;
 
         //Constructor
         public Enemy(Texture2D objectTexture, Rectangle objectBounds) : base(objectTexture, objectBounds) 
         {
             velocity = new Vector2();
             acceleration = new Vector2();
+            velocityDampener = .99f;
         }
 
         public void GetPlayerPosition(Rectangle playerRectangle)
@@ -38,12 +41,13 @@ namespace Nobody_Will_Hear_Them_Scream
             X += (int)velocity.X;
             Y += (int)velocity.Y;
 
+            velocity *= velocityDampener;
             velocity += acceleration;
 
             //Vector2 playerDirFromEnemy = Vector2.Normalize(new Vector2(playerPosition.X - CenterX, playerPosition.Y - CenterY));
             playerDirFromEnemy = Vector2.Normalize(new Vector2(playerPosition.X - X, playerPosition.Y - Y));
 
-            acceleration = 0.15f * playerDirFromEnemy;
+            acceleration = 0.35f * playerDirFromEnemy;
         }
     }
 }
