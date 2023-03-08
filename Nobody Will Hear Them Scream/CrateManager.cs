df6c222863b23c40fef8b2b81fad34b20975b944
Anthony Curtis <amc5224@rit.edu>
2023-03-08 16:16:31 -0500

    added enemy and crate managers

---
+++ Nobody Will Hear Them Scream/CrateManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobody_Will_Hear_Them_Scream
+{
+    internal class CrateManager
+    {
+    }
+}
